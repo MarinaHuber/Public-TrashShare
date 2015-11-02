7743baf885522102144c689b898a631f6e1580a8
Alex Vuijsje <alexvuijsje@MacBook-Pro-van-Alex.fritz.box>
2015-11-02 12:15:49 +0100

    first commit

---
+++ Trashare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trashare
+//
+//  Created by Alex Vuijsje on 02-11-15.
+//  Copyright © 2015 The App Academy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
