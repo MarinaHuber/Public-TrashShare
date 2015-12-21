5bba3412b7c9e01b83717fbf483a27abb29cd789
Marina Huber <marina@Marinas-Air.lan>
2015-12-21 21:13:24 +0100

    initial commit

---
+++ TrashareCell.m
@@ -0,0 +1,28 @@
+//
+//  TrashareCell.m
+//  Trashare
+//
+//  Created by Marina Huber on 11/2/15.
+//  Copyright © 2015 The App Academy. All rights reserved.
+//
+
+#import "TrashareCell.h"
+#import <Parse/Parse.h>
+#import <UIKit/UIKit.h>
+
+@implementation TrashareCell
+
+//what is this?
+- (IBAction)showImage:(id)sender
+{
+    if (self.actionBlock) {
+        self.actionBlock();
+    }
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+}
+@end
---
+++ TrashareData.m
@@ -0,0 +1,14 @@
+//
+//  TrashareData.m
+//  Trashare
+//
+//  Created by Marina Huber on 11/4/15.
+//  Copyright © 2015 The App Academy. All rights reserved.
+//
+
+#import "TrashareData.h"
+
+@implementation TrashareData 
+
+
+@end
