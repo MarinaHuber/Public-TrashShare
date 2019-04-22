0fb22101eed33146fd1715046da9fbf5408f6c81
Marina Huber <marina@pc-4-200.customer.leipzig.basecamp.lan>
2019-04-23 00:19:38 +0200

    Add Fix for detailVC image download

--- Trashare/ViewController/DetailViewController.swift
+++ Trashare/ViewController/DetailViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import Parse
+// TODO: Find localized string solution for all the strings for Parse
 
 @objc class DetailViewController: UIViewController, UINavigationControllerDelegate, UIScrollViewDelegate {
 	@IBOutlet weak var gradientView: UIView!
@@ -16,17 +16,43 @@ import UIKit
 
 	// PFFileObject representes a file of binary data stored on the Parse servers.
 	@IBOutlet var showImage: UIImageView!
-	@IBOutlet var titleTrash: UILabel!
+	@IBOutlet private var titleTrash: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setGradientBackground()
 		if let object = currentObject {
-			titleTrash.text = object["titieTrash"] as? String
+			self.titleTrash.text = object["titleTrashare"] as? String
 		if let fileImage = object["imageFile"] as? PFFileObject {
-			getImageFromData(dataImage: fileImage)
+			getImageFromData(data: fileImage)
 			}
 		}
 	}
+
+	func getImageFromData(data: PFFileObject) {
+		data.getDataInBackground {(data: Data?, error: Error?) in
+			if let data = data, let image = UIImage(data: data) {
+				self.showImage.image = image
+			} else {}
+		}
+	}
+
+
+	func setGradientBackground() {
+		let colorTop =  UIColor(red: 209.0/255.0, green: 255.0/255.0, blue: 100.0/255.0, alpha: 1.0).cgColor
+		let colorBottom = UIColor(red: 174.0/255.0, green: 255.0/255.0, blue: 158.0/255.0, alpha: 1.0).cgColor
+
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [colorTop, colorBottom]
+		gradientLayer.locations = [0.7, 1.0]
+		gradientLayer.frame = self.view.bounds
+
+		self.gradientView.layer.insertSublayer(gradientLayer, at: 0)
+	}
+
+	@IBAction func cancel(_ sender: Any) {
+		navigationController?.popViewController(animated: true)
+	}
+
 
 }
